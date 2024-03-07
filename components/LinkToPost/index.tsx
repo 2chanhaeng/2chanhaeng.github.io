9f38d6d6690cae23df60df836e700b900ba8479f
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-03-07 10:26:20 +0000

    fix: remove code to change path

--- components/LinkToPost/index.tsx
+++ components/LinkToPost/index.tsx
@@ -1,8 +1,7 @@
-import { Base64Path } from "@/types/path";
 import Link from "next/link";
 
-export default function LinkToPost({ encoded, decoded }: Base64Path) {
-  return <Link href={encodeURI(encoded)}>{getLastOfPath(decoded)}</Link>;
+export default function LinkToPost({ path }: { path: string }) {
+  return <Link href={path}>{getLastOfPath(path)}</Link>;
 }
 
 const getLastOfPath = (path: string) => path.split("/").at(-1) ?? "";
--- components/PostList/index.tsx
+++ components/PostList/index.tsx
@@ -1,41 +1,23 @@
 import fs from "fs";
-import { Base64Path } from "@/types/path";
-import { encodeB64 } from "@/utils/b64";
 import LinkToPost from "../LinkToPost";
 import { getAllChildrenDirectoryRecursive } from "./actions";
 
 export default function PostList() {
-  const rawPosts = getAllChildrenDirectoryRecursive("app/posts")
+  const posts = getAllChildrenDirectoryRecursive("app/posts")
     .filter(exceptAppPosts)
     .filter(hasPageFile)
-    .map((path) => {
-      const paths = path.split("/");
-      const last = paths.pop() ?? "";
-      paths.push(encodeB64(last));
-      return { encoded: paths.join("/"), decoded: path };
-    })
     .sort()
-    .reverse();
-  rawPosts.forEach(({ encoded, decoded }) => {
-    try {
-      fs.renameSync(decoded, encoded);
-    } catch (e) {
-      console.error(`Error renaming ${decoded} to ${encoded}: ${e}`);
-    }
-  });
-  const posts = rawPosts.map(removeAppPaths).map(({ encoded, decoded }) => (
-    <li key={decoded}>
-      <LinkToPost encoded={encoded} decoded={decoded} />
-    </li>
-  ));
+    .reverse()
+    .map(removeApp)
+    .map((path) => (
+      <li key={path}>
+        <LinkToPost path={path} />
+      </li>
+    ));
   return <ul>{posts}</ul>;
 }
 
 const removeApp = (path: string) => path.replace("app/", "");
-const removeAppPaths = ({ encoded, decoded }: Base64Path) => ({
-  encoded: removeApp(encoded),
-  decoded: removeApp(decoded),
-});
 const exceptAppPosts = (path: string) => path !== "app/posts";
 const hasPageFile = (path: string): boolean =>
   fs.existsSync(`${path}/page.tsx`) || fs.existsSync(`${path}/page.mdx`);
