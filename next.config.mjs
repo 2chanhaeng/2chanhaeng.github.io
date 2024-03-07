ffe6adc0a28988e0f0be7c8a2939405c8e3834e1
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-03-07 18:14:33 +0900

    chore: add MDX plugins

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 import withMDX from "@next/mdx";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import remarkFrontmatter from "remark-frontmatter";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -6,4 +9,9 @@ const nextConfig = {
   output: "export",
 };
 
-export default withMDX()(nextConfig);
+export default withMDX({
+  options: {
+    remarkPlugins: [rehypeKatex],
+    rehypePlugins: [remarkMath, remarkFrontmatter],
+  },
+})(nextConfig);
