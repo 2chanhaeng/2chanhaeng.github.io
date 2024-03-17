f6d8a18c61623762608901935ed521d28a7357e2
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-03-18 03:42:04 +0900

    feat: change date format

--- components/date-formatter.tsx
+++ components/date-formatter.tsx
@@ -1,12 +1,11 @@
-import { parseISO, format } from "date-fns";
-
 type Props = {
   dateString: string;
 };
 
 const DateFormatter = ({ dateString }: Props) => {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "LLLL	d, yyyy")}</time>;
+  const date = new Date(dateString);
+  const showDate = new Intl.DateTimeFormat().format(date);
+  return <time dateTime={dateString}>{showDate}</time>;
 };
 
 export default DateFormatter;
