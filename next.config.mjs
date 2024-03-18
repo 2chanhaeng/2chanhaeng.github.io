a0eb142a845087a740c5f02119d59fc9bb18ec3e
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-03-18 19:47:14 +0900

    chore: parse mdx with rust

--- next.config.mjs
+++ next.config.mjs
@@ -14,5 +14,6 @@ export default withMDX({
   options: {
     rehypePlugins: [rehypeKatex],
     remarkPlugins: [remarkMath, remarkFrontmatter],
+    experimental: { mdxRs: true },
   },
 })(nextConfig);
