b79f7bf63f20c16f9092cf36ce898a6042daac46
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-03-18 19:46:38 +0900

    feat: add link to posts in home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Intro } from "@/components/intro";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Intro />
+      <Link href="/posts">Posts</Link>
     </main>
   );
 }
