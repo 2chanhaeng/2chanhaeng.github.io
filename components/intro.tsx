47808027bbb2c749de7a45215176280304e8a0ce
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-03-18 18:53:04 +0900

    refac: set blog name as constant

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,8 +1,10 @@
+import { BLOG_NAME } from "@/lib/constants";
+
 export function Intro() {
   return (
     <section className="flex-col flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Chomu&apos;s Blog.
+        {BLOG_NAME}.
       </h1>
       <h4 className="text-center text-lg mt-5 md:pl-8">
         이사 중...{" "}
