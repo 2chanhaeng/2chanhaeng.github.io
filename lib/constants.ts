4b2fa4f8e9d3473203db2d5b2646289e00a1ac63
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-03-20 08:48:12 +0900

    feat: set constants for repository

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,5 @@
 export const BLOG_NAME = "Chomu's Blog";
-export const REPOSITORY_URL =
-  "https://github.com/2chanhaeng/2chanhaeng.github.io";
+export const REPOSITORY_OWNER = "2chanhaeng";
+export const REPOSITORY_NAME = "2chanhaeng.github.io";
+export const REPOSITORY = `${REPOSITORY_OWNER}/${REPOSITORY_NAME}`;
+export const REPOSITORY_URL = `https://github.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}`;
