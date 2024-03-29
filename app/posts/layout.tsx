5951ef2b4b2f833c85661c2b0b813bccae3692dc
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-03-29 20:16:59 +0900

    feat: limit max width after title in post

--- app/posts/layout.tsx
+++ app/posts/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import { Comments } from "@/components/Comments";
 import Container from "@/components/container";
 import Header from "@/components/Header";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Posts",
