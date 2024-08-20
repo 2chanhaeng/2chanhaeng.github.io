61f20835ac28022fbdc73ed85ddd668c899a9d12
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-08-20 11:12:19 +0900

    fix: fix /yt/kpop redirect

---
+++ app/yt/kpop/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function KpopPage() {
+  redirect("https://music.youtube.com/playlist?list=PLz1W1oJooomVgGxRPFwiZ_idCa_3GPylg");
+}
--- app/yt/kpop/route.ts
+++
@@ -1,3 +0,0 @@
-import { redirect } from "next/navigation";
-
-export const GET = () => redirect("https://music.youtube.com/playlist?list=PLz1W1oJooomVgGxRPFwiZ_idCa_3GPylg");
