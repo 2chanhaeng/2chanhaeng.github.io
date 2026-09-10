26ac2354b72e35c12ca27154e27978f15ed9eab4
ChanHaeng Lee <61987505+2chanhaeng@users.noreply.github.com>
2026-09-10 19:16:43 +0900

    Update choco command
    Add mise, firacode and remove figa

--- app/choco/page.tsx
+++ app/choco/page.tsx
@@ -3,7 +3,7 @@ import ClickToCopy from "@/components/ClickToCopy";
 const commands = [
   "Set-ExecutionPolicy Unrestrict",
   "[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))",
-  "choco install firefox git vscode gh github-desktop nvm python figma discord -y --ignore-checksums",
+  "choco install firefox git vscode gh github-desktop mise firacode discord -y --ignore-checksums",
 ];
 export default async function ChocoPage() {
   return (
